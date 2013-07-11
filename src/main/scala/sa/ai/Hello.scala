6485f219fe04e61121178bc345a842d959540526
alexoooo <alex@ostrovsky.biz>
2013-07-10 20:48:20 -0400

    * Adding basic scala and testing libs

---
+++ src/main/scala/sa/ai/Hello.scala
@@ -0,0 +1,12 @@
+package sa.ai
+
+/**
+ * 10/07/13 8:17 PM
+ */
+object Hello extends App
+{
+  val subject = "world"
+
+
+  println("Hello, world")
+}
---
+++ src/test/scala/sa/ai/HelloTest.scala
@@ -0,0 +1,21 @@
+package sa.ai
+
+//import org.specs2.mutable.Specification
+
+/**
+ * Sample
+ */
+object HelloTest /*extends Specification*/ {
+
+
+//  "World world" should {
+//    "contain 11 characters" in {
+//      "Hello world" must have size(11)
+//    }
+//
+////    "Have subject 'world'" in {
+////      Hello.subject === "world"
+////    }
+//  }
+
+}

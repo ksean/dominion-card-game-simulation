536134d3d21e7152630b8b12bd19c0d84c34d574
alexoooo <alex@ostrovsky.biz>
2013-07-13 21:19:58 -0400

    https://github.com/ksean/operation-d/issues/1 * Added Basic Card Copper Count Test

--- src/main/scala/sa/ai/model/card/Basic.scala
+++ src/main/scala/sa/ai/model/card/Basic.scala
@@ -8,3 +8,13 @@ case class Basic(
   silver : Pile,
   gold   : Pile
 )
+
+object Basic {
+  val initialSetForTwoPlayers =
+    Basic(
+      copper = Pile(Card.Copper, 20),
+      silver = Pile(Card.Silver, 20),
+      gold = Pile(Card.Gold, 20)
+    )
+
+}
---
+++ src/test/scala/sa/ai/model/BasicSpec.scala
@@ -0,0 +1,40 @@
+package sa.ai.model
+
+import org.specs2.mutable.SpecificationWithJUnit
+import sa.ai.model.card.{Basic, Card}
+
+/**
+ * 13/07/13 9:06 PM
+ */
+class BasicSpec extends SpecificationWithJUnit
+{
+  "Basic Cards" should {
+
+    "At the beginning of a two player game" in {
+      val basic = Basic.initialSetForTwoPlayers
+
+      "Include three basic treasures" in {
+
+        "With 46 coppers" in {
+          basic.copper.cards.size must be equalTo 46
+        }
+      }
+    }
+  }
+
+//  "Dominion Card model" should {
+//
+//    "Have a 'First Game' card set" in {
+//      val firstGameCards = Card.FirstGame
+//
+//      "With 10 cards" in {
+//        firstGameCards.size must be equalTo 10
+//      }
+//
+//      "With a 'Smithy'" in {
+//        firstGameCards.map(_.name) must contain("Smithy")
+//        // see: http://dominioncg.wikia.com/wiki/Smithy
+//      }
+//    }
+//  }
+}

2735e75ea335f53f8e3f055e87441d1bacfe7be3
alexoooo <alex@ostrovsky.biz>
2013-07-15 22:11:35 -0400

    https://github.com/ksean/operation-d/issues/2 * Added more checks fo RulesetSpec

--- src/test/scala/sa/ai/rule/RulesetSpec.scala
+++ src/test/scala/sa/ai/rule/RulesetSpec.scala
@@ -2,29 +2,33 @@ package sa.ai.rule
 
 import org.specs2.mutable.SpecificationWithJUnit
 import sa.ai.model.Game
-import sa.ai.model.rule.{Move, Ruleset}
+import sa.ai.model.rule.{Shuffle, Move, Ruleset}
 
 /**
- * 15/07/13 9:38 PM
+ * Dominion rules specification
  */
 class RulesetSpec extends SpecificationWithJUnit {
   "Dominion rules" should {
     val rules = Ruleset
 
-    "In the two-player initial state" in {
+    "Have a two-player initial state" in {
       val initialState = Game.twoPlayerInitialState
 
-      "Should have a set of first moves" in {
-        val firstMoves = rules.actions(initialState)
+      "With a set of first moves" in {
+        val firstMoves : Set[Move] = rules.actions(initialState)
 
         "That contains only one option" in {
           firstMoves.size must be equalTo 1
 
-          "This is the first move" in {
-            val firstMove : Move = firstMoves.iterator.next()
+          "Which is the first move" in {
+            val firstMove = firstMoves.iterator.next()
 
-            "In which first player is next to act" in {
+            "For the first player" in {
               firstMove.playerIndex must be equalTo 0
+            }
+
+            "Requiring a discard pile shuffle" in {
+              firstMove must be (anInstanceOf[Shuffle])
             }
           }
         }

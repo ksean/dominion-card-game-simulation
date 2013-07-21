3e89b40e6fc078fec9b490677e2964d60da85813
alexoooo <alex@ostrovsky.biz>
2013-07-20 20:26:14 -0400

    https://github.com/ksean/operation-d/issues/2 * Made Move abstract

--- src/main/scala/sa/ai/rule/Move.scala
+++ src/main/scala/sa/ai/rule/Move.scala
@@ -3,7 +3,7 @@ package sa.ai.rule
 /**
  * 15/07/13 9:26 PM
  */
-sealed class Move(val playerIndex : Int)
+sealed abstract class Move(val playerIndex : Int)
 
 case class ShuffleDiscardIntoDeck(override val playerIndex : Int) extends Move(playerIndex)
 case class DrawFromDeck(override val playerIndex : Int, cardCount : Int) extends Move(playerIndex)

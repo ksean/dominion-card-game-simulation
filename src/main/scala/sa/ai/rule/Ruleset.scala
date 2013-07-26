d57c9e9bef99c2755fa1a5a2271cd8a3c5bc8b97
Sean Kennedy <kennedymsean+gh@gmail.com>
2013-07-25 20:08:06 -0400

    https://github.com/ksean/operation-d/issues/2 * Added shuffle state transition

--- src/main/scala/sa/ai/rule/Ruleset.scala
+++ src/main/scala/sa/ai/rule/Ruleset.scala
@@ -1,6 +1,7 @@
 package sa.ai.rule
 
-import sa.ai.model.Game
+import sa.ai.model.{Player, Game}
+import sa.ai.model.card.{DiscardPile, Deck}
 
 /**
  * http://riograndegames.com/uploads/Game/Game_278_gameRules.pdf
@@ -12,7 +13,14 @@ object Ruleset
   def transition(state:Game, move:Move) : Game = {
     move match {
       case ShuffleDiscardIntoDeck(playerIndex) => {
-        null
+        val currentPlayers = state.players
+        val transitioningPlayer = currentPlayers(playerIndex)
+        val nextDiscardPile = DiscardPile(Seq())
+        val nextDeck = Deck(transitioningPlayer.discard.cards)
+        val nextPlayerState = Player(nextDiscardPile, nextDeck)
+        val nextPlayers = currentPlayers.updated(playerIndex, nextPlayerState)
+
+        state.copy(players = nextPlayers)
       }
 
       case DrawFromDeck(playerIndex, count) => {
